e7924767aaa098a61653fdbee426aac906bbc242
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2021-05-27 01:16:35 +0200

    Create Example.bat

---
+++ Example.bat
@@ -0,0 +1,12 @@
+@echo off
+
+set title=My GTA backup
+
+set baseFolder=C:\Users\YourName\Games\
+
+set amount=0
+set folders[0]=GrandTheftAuto\
+
+set destinationFolder=D:\Backups\GameSaves\GTA\
+
+call Backupper.bat
